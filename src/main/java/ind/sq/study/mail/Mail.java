6f97c629e387413f71b8698b51b26e2bcbf1704c
Sun Qin <sunqin@maycur.com>
2022-06-06 14:55:32 +0800

    add email test

---
+++ src/main/java/ind/sq/study/mail/Mail.java
@@ -0,0 +1,27 @@
+package ind.sq.study.mail;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Mail {
+    public static void main(String[] args) throws MessagingException {
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.host", "smtp.mxhichina.com");
+        properties.setProperty("mail.smtp.auth", "true");
+        Session session = Session.getInstance(properties);
+        session.setDebug(true);
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("ac-notice@maycur.com"));
+        msg.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress("sunqin@maycur.com"));
+        msg.setSubject("测试邮件", "UTF-8");
+        msg.setText("测试内容");
+        msg.saveChanges();
+        var transport = session.getTransport();
+        transport.connect("ac-notice@maycur.com", "Maycur@2022");
+        transport.sendMessage(msg, msg.getAllRecipients());
+
+    }
+}
