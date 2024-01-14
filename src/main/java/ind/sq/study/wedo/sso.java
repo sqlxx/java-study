b31b955ef074084104678b0b96f22ffcaab05bd9
Sun Qin <sunqin@maycur.com>
2024-01-14 15:12:50 +0800

    sso

--- src/main/java/ind/sq/study/wedo/sso.java
+++ src/main/java/ind/sq/study/wedo/sso.java
@@ -4,12 +4,13 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class sso {
     public static void main(String[] args) {
-        String ssoSecret = "h3bb1whhtuvceyqkblgfimt7tt32lhmp";
+        String ssoSecret = "fkhzx4sojnxc5qxyksqy3da0o76pwvd4xsqpddy4fr8n8gkpnl";
         long timestamp = System.currentTimeMillis();
-        String userId = "10002"; //工号或者邮箱或者手机
-        String ssoId = "vj60hzxupenw4zwpmq9a";
+        String userId = "sq@wedoos.tech"; //工号或者邮箱或者手机
+        String ssoId = "lv9tfoq0quhy3xofoiye";
         String ssoToken = DigestUtils.sha256Hex((ssoSecret + ":" + userId + ":" + timestamp).getBytes());
-        String baseUrl = "http://localhost:5173";
+        // String baseUrl = "http://localhost:5173";
+        String baseUrl = "https://app-dev.wedobot.com";
         String url = baseUrl + "/auth/sso?userId=" + userId + "&ssoId=" + ssoId + "&timestamp=" + timestamp + "&ssoToken=" + ssoToken;
         System.out.println(url);
 
